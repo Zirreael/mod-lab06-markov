7efe4b976fdaa6c573ca22bb4ffb47d95c77dd22
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-04-29 15:41:42 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -17,7 +17,7 @@ TEST(TestCaseName2, Test2) {
   Markov gen = Markov(text, npref);
   prefix pref = prefix();
   pref.push_back("one");
-	pref.push_back("fish");
+  pref.push_back("fish");
   std::string s = gen.statetab[pref][0];
   EXPECT_EQ(s, "two");
 }
@@ -30,4 +30,36 @@ TEST(TestCaseName3, Test3) {
   pref.push_back("two");
   std::string s = gen.statetab[pref][0];
   EXPECT_EQ(s, "fish");
+}
+
+TEST(TestCaseName4, Test4) {
+  std::string text = "one fish two fish red fish blue fish";
+  int npref = 1;
+  Markov gen = Markov(text, npref);
+  prefix pref = prefix();
+  pref.push_back("fish");
+  suffix suf = suffix();
+  suf.push_back("two");
+  suf.push_back("red");
+  suf.push_back("blue");
+  EXPECT_EQ(geb.statetab[pref][0], suf[0]);
+  EXPECT_EQ(geb.statetab[pref][1], suf[1]);
+  EXPECT_EQ(geb.statetab[pref][2], suf[2]);
+}
+
+TEST(TestCaseName5, Test5) {
+  auto table = std::map<prefix, suffix>();
+  prefix pref = prefix();
+  suffix suf = suffix();
+  pref.push_back("fish");
+  suf.push_back("red");
+  table[pref] = suf;
+  pref.clear();
+  suf.clear();
+  pref.push_back("red");
+  suf.push_back("fish");
+  table[pref] = suf;
+  Markov gen = Markov(table);
+  std::string res = "fish red fish red fish red fish red fish red fish";
+  EXPECT_EQ(res, gen.Generator(50));
 }
