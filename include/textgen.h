c90cf6559b5010e84747b75cb962b0642ee2df9c
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-04-29 16:00:05 +0300

    Update textgen.h

--- include/textgen.h
+++ include/textgen.h
@@ -12,10 +12,10 @@ typedef std::vector<std::string> suffix;
 
 class Markov {
   public:
-  std::map<prefix, suffix> statetab;
-  explicit Markov(std::string text, int nPref);
-  Markov(std::map<prefix, suffix> table);
-  std::string Generator(int maxLen);
+    std::map<prefix, suffix> statetab;
+    explicit Markov(std::string text, int nPref);
+    explicit Markov(std::map<prefix, suffix> table);
+    std::string Generator(int maxLen);
 };
 
 std::vector<std::string> split(std::string str, std::string delimiter);
