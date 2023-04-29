ae135251ea1eec92fd3ea651f00990ab3e2c7165
Zirreael <126812503+Zirreael@users.noreply.github.com>
2023-04-29 15:55:08 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,20 @@
 #include "textgen.h"
 
 int main() {
-	const int NPREF = 2;
-	const int MAXGEN = 1000;
-	setlocale(LC_ALL, "Russian");
-	std::string text = " ";
-	std::string line;
-	std::ifstream fs("text.txt");
-	if (fs.is_open()) {
-		while (getline(fs, line)) {
-			text += line;
-		}
-	} else {
-		std::cout << "file dont open";
-	}
-	fs.close();
-	Markov gen = Markov(text, NPREF);
-	std::cout << gen.Generator(MAXGEN);
+  const int NPREF = 2;
+  const int MAXGEN = 1000;
+  setlocale(LC_ALL, "Russian");
+  std::string text = " ";
+  std::string line;
+  std::ifstream fs("text.txt");
+  if (fs.is_open()) {
+    while (getline(fs, line)) {
+      text += line;
+    }
+  } else {
+    std::cout << "file dont open";
+  }
+  fs.close();
+  Markov gen = Markov(text, NPREF);
+  std::cout << gen.Generator(MAXGEN);
 }
